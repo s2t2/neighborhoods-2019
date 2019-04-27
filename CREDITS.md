3390f84a09d1711b4b57a43e4cd5be6a91a2f867
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-27 18:34:32 -0400

    Prepare to parse the response

--- CREDITS.md
+++ CREDITS.md
@@ -1,4 +1,12 @@
 
+# Credits, Notes, and References
+
+## Zillow
+
   + https://www.zillow.com/howto/api/neighborhood-boundaries.htm
   + https://www.zillowstatic.com/static-neighborhood-boundaries/LATEST/static-neighborhood-boundaries/shp/ZillowNeighborhoods-CT.zip
-  + http://mapstarter.com/
+  + https://www.zillow.com/howto/api/GetRegionChildren.htm
+
+## Soup
+
+  + https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/beautifulsoup.md
--- my_script.py
+++ my_script.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+from bs4 import BeautifulSoup
 
 load_dotenv()
 
@@ -8,13 +9,13 @@ API_KEY = os.environ.get("ZILLOW_API_KEY", "OOPS")
 
 state = "ct"
 city = "new%20haven" # todo: use some package or module (requests?) to convert this from human-friendly input "New Haven"
-
 url = f"https://www.zillow.com/webservice/GetRegionChildren.htm?zws-id={API_KEY}&state={state}&city={city}&childtype=neighborhood"
-
 print("URL", url)
 
 response = requests.get(url)
-
 print("RESPONSE", response.status_code, type(response))
-
 print(response.text)
+
+soup = BeautifulSoup(response.text)
+
+breakpoint()
